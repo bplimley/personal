df6b12b21a255cf736d74484d87588d7e963696c
Brian Plimley <as.white.as.snow@gmail.com>
2016-01-06 21:52:59 -0800

    abcde.py: AlbumInstance.split_exts() and more

--- abcde.py
+++ abcde.py
@@ -21,6 +21,8 @@ def main():
     albumdir = '/home/plimley/Music/new CDs/Sara Bareilles - Little Voice'
     a = AlbumInstance(albumdir)
 
+    album_list = get_album_list('/home/plimley/Music/new CDs')
+
 
 class FileInstance(object):
     """
@@ -30,9 +32,10 @@ class FileInstance(object):
     def __init__(self, filepath):
         """
         """
+        self.fullpath = filepath
         self.path, self.filename = os.path.split(filepath)
         self.name, ext = os.path.splitext(self.filename)
-        self.ext = ext[1:]
+        self.ext = ext[1:]      # drop the .
 
         self.stat = os.stat(filepath)
 
@@ -57,6 +60,8 @@ class AlbumInstance(object):
         """
 
         self.location = filepath
+        self.get_name()
+
         # filelist does not include the path
         self.filelist = [FileInstance(os.path.join(filepath, s))
                          for s in os.listdir(filepath)]
@@ -77,12 +82,71 @@ class AlbumInstance(object):
             self.track_nums = set([f.track_num for f in self.musiclist])
             self.n_tracks = len(self.track_nums)
 
+    def get_name(self):
+        head, tail = os.path.split(self.location)
+        if tail == '':
+            _, tail = os.path.split(head)
+        self.name = tail
+
+    def split_exts(self, target_dir, rm_original=False):
+        """
+        For album located in src_dir/album_dir:
+          copy src_dir/album_dir/*.mp3 to target_dir/mp3/album_dir/*.mp3
+        and similarly for each extension type.
+        """
+
+        if not all(self.has_ext.values()):
+            raise AbcdeException
+
+        for ftype in EXTS:
+            this_dest_dir = os.path.join(target_dir, ftype, self.name)
+            for f in self.list_by_ext[ftype]:
+                shutil.copy2(f.fullpath,
+                             os.path.join(this_dest_dir, f.filename))
+                if rm_original:
+                    self.del_file(f)
+
+    def del_file(self, file_obj):
+        """
+        Remove file file_obj, also removing references in the album lists.
+        file_obj is a FileInstance object.
+        """
+
+        if file_obj not in self.filelist:
+            raise AbcdeException
+
+        self.filelist.remove(file_obj)
+        for ftype in EXTS:
+            if file_obj in self.list_by_ext[ftype]:
+                self.list_by_ext[ftype].remove(file_obj)
+        os.remove(file_obj.fullpath)
+
     def find_duplicate_tracks(self):
         """
         Examine album files for duplicate tracks with varied names.
         """
 
         pass
+
+    def __str__(self):
+        return self.location
+
+
+def get_album_list(dirname):
+    """
+    Return a list of albums from the contents of directory, dirname.
+    """
+
+    album_dir_list = [os.path.join(dirname, d) for d in os.listdir(dirname)]
+    album_dir_list.sort()
+
+    album_list = [AlbumInstance(a) for a in album_dir_list]
+
+    return album_list
+
+
+class AbcdeException(Exception):
+    pass
 
 
 if __name__ == '__main__':
